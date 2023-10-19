b8423c81f0874de6debc715b079d8c075eecd61b
jeffbalailvu <142818489+jeffilvu@users.noreply.github.com>
2023-10-19 14:11:18 -0300

    ++
    Exercicio de senha embaralhada

---
+++ exercicios2.py
@@ -0,0 +1,21 @@
+# iterando strings com while
+#      012345678910
+
+import random
+import string
+
+senha= input('digite sua senha: ') 
+#     -11...
+
+indice = 0
+nova_senha = ''
+senhaEscondida = ''
+while indice < len(senha):
+    aleatorio = random.choice(string.ascii_letters)
+    letra = senha[indice]
+    nova_senha += f'{aleatorio}{letra}'
+    indice+=1
+    senhaEscondida = nova_senha[::-1]
+
+
+print(f'Senha escondida: {senhaEscondida}')
