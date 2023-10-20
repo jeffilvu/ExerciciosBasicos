6bbc0ca6398cc2bace8babb553a93eb6b0780571
jeffbalailvu <142818489+jeffilvu@users.noreply.github.com>
2023-10-20 14:19:43 -0300

    ++

--- exercicios3.py
+++ exercicios3.py
@@ -1,33 +1,52 @@
 # clculadora com o while
 
 sair = "n"
+resultado = ''
+
+
 
 while sair == "n":
+        
+        num1 = input("digite o primeiro valor: ")
+        num2 = input("digite o segundo valor: ")
+        operador = input("digite o operador (/, * , -, +): ")
 
-    num1 = input("digite o primeiro valor: ")
-    num2 = input("digite o segundo valor: ")
-    operador = input("digite o operador (/, * , -, +): ")
-    num1 = float(num1)
-    num2 = float(num2)
-    while operador == "/":
+        try:
+            num1 = float(num1)
+            num2 = float(num2)
+            numvalidos = True
 
-        resultado = num1/num2
-        break
-
-    while operador == "*":
-
-        resultado = num1*num2
-        break
-
-    while operador == "+":
-        resultado = num1+num2
-        break
-
-    while operador == "-":
-        resultado = num1-num2
-        break
-
-    print(resultado)
-    sair = input("deseja sair? \"s\" ou \"n\": ")
+        except:
+            numvalidos = False
 
 
+
+
+        if numvalidos:
+
+            while operador == "/":
+
+                resultado = num1/num2
+                break
+
+            while operador == "*":
+
+                resultado = num1*num2
+                break
+
+            while operador == "+":
+                resultado = num1+num2
+                break
+
+            while operador == "-":
+                resultado = num1-num2
+                break
+
+
+
+        else:
+            print("algum numero digitado é invalido")
+            sair = "n"
+
+        print(resultado)
+        sair = input("deseja sair? \"s\" ou \"n\": ")
